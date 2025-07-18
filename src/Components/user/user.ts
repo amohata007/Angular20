832759e6d981c0a83241f08fe32cae88be888f9a
amohata007 <amohata.007@gmail.com>
2025-07-18 18:20:55 +0530

    get API

--- src/Components/user/user.ts
+++ src/Components/user/user.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrl: './user.css',
 })
-export class User {
+export class User implements OnInit {
+  http = inject(HttpClient);
+  userLists: any[] = [];
 
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((res: any) => {
+        this.userLists = res;
+      });
+  }
 }
