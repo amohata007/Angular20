1539bfe7d980238c69cd08b7283cb720ef0752b9
amohata007 <amohata.007@gmail.com>
2025-07-18 18:00:41 +0530

    Pipes, Lifecycle

--- src/Components/control/control.ts
+++ src/Components/control/control.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { DatePipe, UpperCasePipe } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control',
-  imports: [FormsModule],
+  imports: [FormsModule, UpperCasePipe, DatePipe],
   templateUrl: './control.html',
   styleUrl: './control.css',
 })
-export class Control {
+export class Control implements OnInit, OnDestroy {
   isVisisble: boolean = true;
   shortMonth: string = 'jan';
   users: string[] = ['Virat', 'Dhoni', 'Rohit', 'Bumrah'];
+  date: number = Date.now();
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('On Init');
+  }
 
   showData() {
     this.isVisisble = true;
@@ -18,5 +28,9 @@ export class Control {
 
   hideData() {
     this.isVisisble = false;
+  }
+
+  ngOnDestroy(): void {
+    console.log('On Destroy');
   }
 }
