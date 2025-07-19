048a95a8c45684144153ad841fe9c7bd3db8de52
amohata007 <amohata.007@gmail.com>
2025-07-19 15:01:31 +0530

    Post API

--- src/Components/user/user.ts
+++ src/Components/user/user.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User implements OnInit {
   http = inject(HttpClient);
   userLists: any[] = [];
+
+  obj: any = {
+    userId: 0,
+    emailId: '',
+    password: '',
+    fullName: '',
+    mobileNo: '',
+  };
 
   ngOnInit(): void {
     this.getUsers();
@@ -20,6 +29,14 @@ export class User implements OnInit {
       .get('https://jsonplaceholder.typicode.com/users')
       .subscribe((res: any) => {
         this.userLists = res;
+      });
+  }
+
+  saveInfo() {
+    this.http
+      .post('https://www.freeprojectapi.com/api/GoalTracker/register', this.obj)
+      .subscribe((res: any) => {
+        console.log(res, 'data');
       });
   }
 }
